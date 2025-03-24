f4088fb94ab83f2948d0e0971a34ba8142ef3490
alanmoraales <alanmoraales845@gmail.com>
2025-03-24 17:31:50 -0600

    add env config for client

--- apps/web/lib/imsClient.ts
+++ apps/web/lib/imsClient.ts
@@ -1,5 +1,6 @@
-import Client, { Local } from '@repo/ims-client';
+import Client from '@repo/ims-client';
+import environment from './environment';
 
-const client = new Client(Local);
+const client = new Client(environment.NEXT_PUBLIC_IMS_API_URL);
 
 export default client;

86f80c7fb7118b468ee546e209bd72dacde9c5af
alanmoraales <alanmoraales845@gmail.com>
2025-04-24 14:06:48 -0600

    add country details params hooks

---
+++ apps/web/app/countries/hooks/use-country-details-params.ts
@@ -0,0 +1,18 @@
+import { useParams } from 'next/navigation';
+import { Params } from 'next/dist/server/request/params';
+import isNumber from '@/utils/is-number';
+
+interface CountryPageParams extends Params {
+  id: string;
+}
+
+export default function useCountryDetailsParams() {
+  const params = useParams<CountryPageParams>();
+  const countryId = parseInt(params.id, 10);
+  const isValidId = isNumber(params.id);
+
+  return {
+    countryId,
+    isValidId,
+  };
+}

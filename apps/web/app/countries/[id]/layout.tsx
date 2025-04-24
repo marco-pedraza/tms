e41c60a375f7ca32703cb9f40dc6b1a1fe8523db
alanmoraales <alanmoraales845@gmail.com>
2025-04-24 14:44:07 -0600

    remove console log

--- apps/web/app/countries/[id]/layout.tsx
+++ apps/web/app/countries/[id]/layout.tsx
@@ -1,7 +1,7 @@
 'use client';
 
-import useQueryCountry from '@/countries/hooks/use-query-country';
 import CountryNotFound from '@/countries/components/country-not-found';
+import useQueryCountry from '@/countries/hooks/use-query-country';
 import useCountryDetailsParams from '@/countries/hooks/use-country-details-params';
 
 export default function CountryLayout({
@@ -10,21 +10,18 @@ export default function CountryLayout({
   children: React.ReactNode;
 }) {
   const { countryId, isValidId } = useCountryDetailsParams();
-  const { data, status, error } = useQueryCountry({
+  const { status, error } = useQueryCountry({
     countryId,
     enabled: isValidId,
   });
-  console.log({ data, status, error });
   // @ts-expect-error - error is not defined in the type
   const isCountryNotFound = !isValidId || error?.code === 'not_found';
 
   if (isCountryNotFound) {
-    console.log('Country not found from layout');
     return <CountryNotFound />;
   }
 
   if (status === 'error') {
-    console.log('Country fetch error from layout');
     return <div>Error</div>;
   }
 

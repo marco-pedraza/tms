654d9afcd3b6c42325fa3112fa38e7738d43fe7b
alanmoraales <alanmoraales845@gmail.com>
2025-04-24 12:34:27 -0600

    add useQueryCountry

---
+++ apps/web/app/countries/hooks/use-query-country.ts
@@ -0,0 +1,39 @@
+import imsClient from '@/lib/imsClient';
+import type { countries } from '@repo/ims-client';
+import {
+  useQuery,
+  useQueryClient,
+  UseQueryResult,
+} from '@tanstack/react-query';
+
+interface UseQueryCountryProps {
+  countryId: number;
+  enabled?: boolean;
+}
+
+/**
+ * Custom hook for querying a country by ID.
+ *
+ * This hook provides a reusable query for fetching a country by its ID.
+ * It handles query setup, caching, and error handling.
+ */
+export default function useQueryCountry({
+  countryId,
+  enabled = true,
+}: UseQueryCountryProps): UseQueryResult<countries.Country, Error> {
+  const queryClient = useQueryClient();
+  const query = useQuery({
+    queryKey: ['country', countryId],
+    enabled,
+    queryFn: () => imsClient.inventory.getCountry(countryId),
+    initialData: () =>
+      queryClient
+        ?.getQueryData<countries.PaginatedCountries>(['countries'])
+        ?.data.find((country) => country.id === countryId),
+    initialDataUpdatedAt: () =>
+      queryClient?.getQueryState<countries.Country[]>(['countries'])
+        ?.dataUpdatedAt,
+  });
+
+  return query;
+}

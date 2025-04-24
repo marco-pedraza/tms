8ea016a6edbe06c68a9014804995d5cb24b870df
alanmoraales <alanmoraales845@gmail.com>
2025-04-24 14:06:28 -0600

    add country details layout

---
+++ apps/web/app/countries/[id]/layout.tsx
@@ -0,0 +1,32 @@
+'use client';
+
+import useQueryCountry from '@/app/countries/hooks/use-query-country';
+import CountryNotFound from '@/app/countries/components/country-not-found';
+import useCountryDetailsParams from '@/app/countries/hooks/use-country-details-params';
+
+export default function CountryLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const { countryId, isValidId } = useCountryDetailsParams();
+  const { data, status, error } = useQueryCountry({
+    countryId,
+    enabled: isValidId,
+  });
+  console.log({ data, status, error });
+  // @ts-expect-error - error is not defined in the type
+  const isCountryNotFound = !isValidId || error?.code === 'not_found';
+
+  if (isCountryNotFound) {
+    console.log('Country not found from layout');
+    return <CountryNotFound />;
+  }
+
+  if (status === 'error') {
+    console.log('Country fetch error from layout');
+    return <div>Error</div>;
+  }
+
+  return children;
+}

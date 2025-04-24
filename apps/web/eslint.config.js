4954846d0342906a80fa266fb2ffd4fa17d0e551
alanmoraales <alanmoraales845@gmail.com>
2025-04-24 14:23:55 -0600

    add prettier auto imports configuration

--- apps/web/eslint.config.js
+++ apps/web/eslint.config.js
@@ -1,5 +1,5 @@
-import { nextJsConfig } from '@repo/eslint-config/next-js';
 import pluginQuery from '@tanstack/eslint-plugin-query';
+import { nextJsConfig } from '@repo/eslint-config/next-js';
 
 /** @type {import("eslint").Linter.Config} */
 export default [...nextJsConfig, ...pluginQuery.configs['flat/recommended']];

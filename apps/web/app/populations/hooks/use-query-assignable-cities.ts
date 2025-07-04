cec9d1b837d041060c29f6ec4266d87b56c7daa7
Alan Morales <alanmoraales845@gmail.com>
2025-07-04 14:07:05 -0600

    disable refetching for assignable cities query (#184)

--- apps/web/app/populations/hooks/use-query-assignable-cities.ts
+++ apps/web/app/populations/hooks/use-query-assignable-cities.ts
@@ -18,5 +18,16 @@ export default function useQueryAssignableCities({
       imsClient.inventory.listAvailableCities({
         populationId,
       }),
+    /**
+     * Edge case: The user selects a city from the assignable cities list,
+     * But then, the assignable cities at the server change, removing the selected city from the list.
+     * However, the selected city id is still at the form state, so the form will try to submit with an invalid city id.
+     *
+     * To avoid this, we are disabling all refetching. So the form will submit with an invalid city id
+     * but the server will reject the request, letting the user know that the city is not assignable anymore.
+     */
+    refetchOnWindowFocus: false,
+    refetchOnMount: false,
+    refetchOnReconnect: false,
   });
 }
